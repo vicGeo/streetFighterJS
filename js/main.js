15279542b36693e1c5c182ae819f48a6f8923207
vicGeo <vicsanig@alumni.upves>
2021-02-06 10:40:32 +0100

    Instanciamos a los jugadores main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,23 @@ class Fighter {
     }
 
 };
+
+//Instancias y variables globales
+// name, life, strength, defense, luck
+
+let fighter1 = new Fighter("Ryu",250,60,35,7);
+let fighter2 = new Fighter("Ken",250,60,30,6);
+let fighter3 = new Fighter("Chunli",250,40,25,6);
+let fighter4 = new Fighter("Blanka",250,60,20,4);
+let fighter5 = new Fighter("Zangief",250,60,10,4);
+let fighter6 = new Fighter("Guile",250,50,35,6);
+let fighter7 = new Fighter("Honda",250,60,20,3);
+let fighter8 = new Fighter("Dhalsim",250,40,50,5);
+let fighter9 = new Fighter("Balrog",250,60,40,8);
+let fighter10 = new Fighter("Vega",250,55,50,5);
+let fighter11 = new Fighter("Sagat",250,60,50,7);
+let fighter12 = new Fighter("Bison",250,70,70,5);
+
+let player1 = "";
+
+let player2 = "";
