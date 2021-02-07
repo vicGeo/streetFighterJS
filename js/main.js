2755450d1adb65c14a752f9dda67912a73124472
vicGeo <vicsanig@alumni.upves>
2021-02-07 09:45:16 +0100

    Añadimos la funcion hover character a main.js

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ class Fighter {
         this.strength = strength;
         this.defense = defense;
         this.luck = luck;
-        this.handicap = luck - Math.floor(Math.random() * 5);
+        this.handicap = luck - Math.floor(Math.random() * 10);
     }
 
     attack(enemy) {
-        enemy.life -= (this.strength - enemy.defense) * (this.luck - this.handicap);
+        enemy.life -= (this.strength - enemy.defense) - (this.luck - this.handicap);
     }
 
     specialAttack(enemy) {
-        enemy.life -= (this.strength + this.luck) - enemy.defense;
+        enemy.life -= (this.strength - enemy.luck) - enemy.defense;
     }
 
 };
@@ -26,16 +26,16 @@ class Fighter {
 
 let fighter1 = new Fighter("Ryu",250,60,35,7);
 let fighter2 = new Fighter("Ken",250,60,30,6);
-let fighter3 = new Fighter("Chunli",250,40,25,6);
-let fighter4 = new Fighter("Blanka",250,60,20,4);
-let fighter5 = new Fighter("Zangief",250,60,20,4);
-let fighter6 = new Fighter("Guile",250,50,35,6);
-let fighter7 = new Fighter("Honda",250,60,20,3);
-let fighter8 = new Fighter("Dhalsim",250,40,30,5);
-let fighter9 = new Fighter("Balrog",250,60,40,6);
-let fighter10 = new Fighter("Vega",250,55,45,5);
-let fighter11 = new Fighter("Sagat",250,60,50,5);
-let fighter12 = new Fighter("Bison",250,70,50,5);
+let fighter3 = new Fighter("Chunli",250,50,25,6);
+let fighter4 = new Fighter("Blanka",250,60,25,5);
+let fighter5 = new Fighter("Zangief",250,60,25,5);
+let fighter6 = new Fighter("Guile",250,55,35,6);
+let fighter7 = new Fighter("Honda",250,60,25,4);
+let fighter8 = new Fighter("Dhalsim",250,50,30,5);
+let fighter9 = new Fighter("Balrog",250,55,40,3);
+let fighter10 = new Fighter("Vega",250,55,45,3);
+let fighter11 = new Fighter("Sagat",250,60,40,3);
+let fighter12 = new Fighter("Bison",250,70,40,3);
 
 let player1 = "";
 
@@ -68,6 +68,26 @@ const changeScreen = (phase, newPhase) => {
     currentScreen.style.display = "none";
     nextScreen.style.display = "block";
 
+};
+
+//Funcion hover jugador
+
+let hoverCharacter = (character) => {
+    let fighter = allplayers[character];
+    showStatsP1 = document.getElementById("stats");
+    showStatsP2 = document.getElementById("stats");
+
+    if(player1 == "") {
+        showStatsP1.innerHTML = `Name: ${fighter.name} </br>
+                                Strenght: ${fighter.strength} </br>
+                                Defense: ${fighter.defense} </br>
+                                Luck: ${fighter.luck}`;
+    } else {
+        showStatsP2.innerHTML = `Name: ${fighter.name} </br>
+                                Strenght: ${fighter.strength} </br>
+                                Defense: ${fighter.defense} </br>
+                                Luck: ${fighter.luck}`;
+    }
 };
 
 //Función selección de jugador
@@ -133,7 +153,7 @@ const damage = () => {
     healthPlayer2 = document.getElementById("healthPlayer2");
 
     if (turn == 0) {
-        if (superAttack >= 3) {
+        if (superAttack == 5) {
             player1.specialAttack(player2);
             infoAttack.innerHTML = `${player1.name} hits with special attack`;
         } else {
@@ -142,7 +162,7 @@ const damage = () => {
 
         }
     } else {
-        if(superAttack >= 4) {
+        if(superAttack == 1) {
             player2.specialAttack(player1);
             infoAttack.innerHTML = `${player2.name} hits with special attack`;
 
@@ -153,8 +173,8 @@ const damage = () => {
     };
 
 
-    // console.log(player1.life);
-    // console.log(player2.life);
+    console.log(player1.life);
+    console.log(player2.life);
     healthPlayer1.value = `${player1.life}`;
     healthPlayer2.value = `${player2.life}`;
     fighterWin();
@@ -210,4 +230,3 @@ reset.addEventListener('click', () => {
 
 const resolveIn = delay =>
 new Promise(res => setTimeout(() => res(delay), delay));
-
